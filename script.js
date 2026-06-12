b961b5fc1bc9fe60bfaef62af538871419092326
guilherme de oliveira <guiguioliveiragomes099@gmail.com>
2026-06-12 01:17:22 -0300

    AIri V1.1 - interface aprimorada e efeito digitando

--- script.js
+++ script.js
@@ -2,34 +2,91 @@ let campoMensagem =
 document.getElementById ("mensagem");
 let botaoEnviar = document.getElementById ("enviar");
 let chat = document.getElementById ("chat");
-  botaoEnviar.addEventListener("click",function(){
+ 
+let msgBot = document.createElement("p");
+msgBot.textContent = "🌺AIri V1.1\n\nOlá Mestre!\nDigite'ajuda' para ver meus comandos";
+msgBot.classList.add("bot");
+chat.appendChild(msgBot);
+
+//enter
+campoMensagem.addEventListener("keydown",function(event){
+    
+    if (event.key == "Enter"){
+        botaoEnviar.click();
+    }
+});
+
+botaoEnviar.addEventListener("click",function(){
 
     let mensagem = campoMensagem.value;
+   
+//criar mensagem do usuario
+let msgUser = document.createElement ("p");
+msgUser.textContent = "Você:" + mensagem;
+msgUser.classList.add("user");
+chat.appendChild(msgUser);
 
+//resposta da airi
+
+let resposta="";
 if (mensagem=="oi"){
-    chat.textContent = "olá,mestre bem vindo! como posso ajudar hoje? 🌺"
+    resposta = "olá,mestre bem vindo! como posso ajudar hoje? 🌺";
 }
 
 else if (mensagem=="curiosidade"){
-    chat.textContent = "você sabia que Airi é o primeiro projeto do meu criador?"
-}      
+    resposta = "você sabia que Airi é o primeiro projeto do meu criador?";
+}
+
+else if(mensagem=="github"){
+    resposta = "Meu código está disponivel em:\n\https://github.com/guilhermeOLi01/AIriV1\n\nLá você pode acompanhar minha evolução.";
+}
+
+else if (mensagem=="linkedin"){
+    resposta = "Meu criador compartilha sua jornada de aprendizado no linkedIn:\n\www.linkedin.com/in/guilherme-oliveira-55470a410\n\n venha acompanhar sua evolução!";
+}
+
+else if (mensagem=="criador"){
+    resposta = "Meu criador é Guilherme. Fui criada para ajuda-lo a aprender programação e me tornar uma IA cada vez melhor!.🌺";
+}
+
 else if (mensagem=="trabalho"){
-    chat.textContent = "o traballho pode ser cansativo, mestre. mas cada experiência traz aprendizado🌺"
+    resposta = "o traballho pode ser cansativo, mestre. mas cada experiência traz aprendizado🌺";
 }
 
 else if (mensagem=="estudar"){
-   chat.textContent = "estudar é uma ótima forma de evoluir! o que deseja aprender hoje?"
+   resposta = "estudar é uma ótima forma de evoluir! o que deseja aprender hoje?";
 
 }
 
 else if(mensagem=="ajuda"){
-    chat.textContent = "ainda estou aprendendo, mas farei o meu melhor para ajudar"
+    resposta = "\naqui estão Alguns comandos disponiveis da AIri \n\oi\najuda\ncuriosidade\ntrabalho\nestudar\ncriador\ngithub\nlinkedin";
 }
 
 else{
-    chat.textContent = "desculpe,mestre. ainda não aprendi sobre esse assunto"
+    resposta = "desculpe,mestre. ainda não aprendi sobre esse assunto";
 }
 
+let digitando = document.createElement("p");
+digitando.textContent = "AIri está digitando...";
+chat.appendChild(digitando);
+chat.scrollTop = chat.scrollHeight;
+
+setTimeout(function(){
+
+    digitando.remove();
+
+let msgBot = document.createElement("p");
+msgBot.textContent = "AIri:" + resposta;
+msgBot.classList.add("bot");
+chat.appendChild(msgBot);
+
+
+//limpar input
      campoMensagem.value = "";
+    
+//sempre por ultimo     
+    chat.scrollTop = chat.scrollHeight;
+
+},1000);
 
 });
