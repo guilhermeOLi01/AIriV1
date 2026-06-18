33c843e70072732424ffebada667892e5421e174
guilherme de oliveira <guiguioliveiragomes099@gmail.com>
2026-06-18 11:07:23 -0300

    AIri V2 - API e digitando

--- script.js
+++ script.js
@@ -2,7 +2,77 @@ let campoMensagem =
 document.getElementById ("mensagem");
 let botaoEnviar = document.getElementById ("enviar");
 let chat = document.getElementById ("chat");
- 
+//arrayas
+let respostaSaudacao = [
+    "Olá, mestre!🌺",
+    "Bem-vindo novamente!🌺",
+    "Que bom te ver por aqui!🌺"
+];
+
+let respostaEstudar = [
+    "Estudar é uma ótima forma de evoluir!🌺",
+    "O conhecimento abre portas, mestre!🚀",
+    "Cada dia  de estudo te aproxima do seu objetivo.🌺"
+];
+
+let respostaTrabalho = [
+    "O trabalho desenvolve habilidades e abre novas oportunidades. 🚀",
+    "Cada experiência profissional ensina algo valioso para o futuro. 🌺",
+    "Persistência no trabalho costuma trazer resultados a longo prazo. 💼",
+    "Todo profissional começou como iniciante um dia. Contiue evoluindo!🚀"
+];
+
+let respostaCuriosidade = [
+    "🐙 Polvos possuem três corações!",
+    "🦒 A língua de uma girafa pode medir até 50 centímetros.",
+    "🍯 O mel é um dos poucos alimentos que não estraga naturalmente.",
+    "🧠 O cérebro humano possui cerca de 86 bilhões de neurônios."
+];
+//comandos feitos
+let comandos= {
+    oi: respostaSaudacao,
+    estudar: respostaEstudar,
+    trabalho: respostaTrabalho,
+    curiosidade: respostaCuriosidade
+};
+
+function respostaAleatoria(array){
+    let indice =
+    Math.floor(Math.random()* array.length);
+
+    return array[indice];
+}
+
+//api
+
+async function curiosidadeAPI() {
+
+    let resposta = await fetch(
+        "https://catfact.ninja/fact"
+    );
+
+    let dados = await resposta.json();
+
+    return dados.fact;
+    
+}
+
+curiosidadeAPI().then(function(resultado){
+    console.log(resultado);
+});
+
+async function conselhoAPI() {
+    let resposta = await fetch(
+        "https://api.adviceslip.com/advice"
+    );
+
+    let dados = await resposta.json();
+
+    return dados.slip.advice;
+    
+}
+
+//evento botão
 let msgBot = document.createElement("p");
 msgBot.textContent = "🌺AIri V1.1\n\nOlá Mestre!\nDigite'ajuda' para ver meus comandos";
 msgBot.classList.add("bot");
@@ -16,7 +86,7 @@ campoMensagem.addEventListener("keydown",function(event){
     }
 });
 
-botaoEnviar.addEventListener("click",function(){
+botaoEnviar.addEventListener("click", async function(){
 
     let mensagem = campoMensagem.value;
    
@@ -25,16 +95,27 @@ let msgUser = document.createElement ("p");
 msgUser.textContent = "Você:" + mensagem;
 msgUser.classList.add("user");
 chat.appendChild(msgUser);
+chat.scrollTop = chat.scrollHeight;
+
+let digitando = document.createElement("p");
+digitando.textContent = "AIri está digitando...";
+chat.appendChild(digitando);
+
+chat.scrollTop = chat.scrollHeight;
 
 //resposta da airi
 
 let resposta="";
-if (mensagem=="oi"){
-    resposta = "olá,mestre bem vindo! como posso ajudar hoje? 🌺";
+if (mensagem == "curiosidade"){
+    resposta = await curiosidadeAPI();
 }
 
-else if (mensagem=="curiosidade"){
-    resposta = "você sabia que Airi é o primeiro projeto do meu criador?";
+else if (mensagem == "conselho"){
+    resposta = await conselhoAPI();
+}
+
+else if (comandos[mensagem]) {
+    resposta = respostaAleatoria(comandos[mensagem]);
 }
 
 else if(mensagem=="github"){
@@ -49,27 +130,9 @@ else if (mensagem=="criador"){
     resposta = "Meu criador é Guilherme. Fui criada para ajuda-lo a aprender programação e me tornar uma IA cada vez melhor!.🌺";
 }
 
-else if (mensagem=="trabalho"){
-    resposta = "o traballho pode ser cansativo, mestre. mas cada experiência traz aprendizado🌺";
-}
-
-else if (mensagem=="estudar"){
-   resposta = "estudar é uma ótima forma de evoluir! o que deseja aprender hoje?";
-
-}
-
-else if(mensagem=="ajuda"){
-    resposta = "\naqui estão Alguns comandos disponiveis da AIri \n\oi\najuda\ncuriosidade\ntrabalho\nestudar\ncriador\ngithub\nlinkedin";
-}
-
 else{
     resposta = "desculpe,mestre. ainda não aprendi sobre esse assunto";
 }
-
-let digitando = document.createElement("p");
-digitando.textContent = "AIri está digitando...";
-chat.appendChild(digitando);
-chat.scrollTop = chat.scrollHeight;
 
 setTimeout(function(){
 
